20d087f968170947996057f3734d74f10fbbda42
Leland Jordon <lelandjordon@mac.com>
2016-03-29 16:24:36 -0400

    Centered the card; added more notes to pseudocode file.

--- script.js
+++ script.js
@@ -4,11 +4,29 @@ card1 = {
 };
 console.log(card1);
 
+// Build a deck of flash cards ----
+deck1 = []
+function deckAdd(h, e){
+  var card = {hangul: h, english: e}
+  deck1.push(card);
+}
+deckAdd("다", "da");
+deckAdd("두", "doo");
+deckAdd("바", "ba");
+deckAdd("부", "boo");
+console.log(deck1);
+
+titleCard = function(){
+  var el = document.querySelector(".flash_card")
+  el.innerHTML = ("한글")
+}
+titleCard();
+
 displayCard = function(){
   var el = document.querySelector(".flash_card")
   el.innerHTML = (card1.hangul);
 }
-displayCard();
+// displayCard();
 
 var clickCard = document.querySelector(".flash_card")
   clickCard.addEventListener("click", function(){
@@ -20,10 +38,21 @@ displayCard2 = function(){
   var el = document.querySelector(".flashCard")
   el.innerHTML = (card1.hangul);
 }
-displayCard2();
+// displayCard2();
 
-var clickCard2 = document.querySelector(".flashCard")
+
+var clickCard2 = document.querySelector(".cardBottom")
   clickCard2.addEventListener("click", function(){
-    var el = document.querySelector(".flashCard")
+    var el = document.querySelector(".cardBottom")
     el.innerHTML = (card1.english);
+  })
+
+clickCounter = 0;
+var clickCard2 = document.querySelector(".cardBottom")
+  clickCard2.addEventListener("click", function(){
+    var elTop = document.querySelector(".flashCard")
+    elTop.innerHTML = deck1[clickCounter].hangul;
+    var elBottom = document.querySelector(".cardBottom")
+    elBottom.innerHTML = deck1[clickCounter].english;
+    clickCounter++
   })
